f3b083771309406f57ba121969de4b3c72a98e50
Paul Adams <paul.adams@endocode.com>
2017-05-15 16:03:15 +0200

    drush: simplest. plan. ever.

---
+++ plan.sh
@@ -0,0 +1,32 @@
+pkg_name=drush
+pkg_origin=baggerspion
+pkg_version="8"
+pkg_maintainer="Paul Adams <paul@baggerspion.net>"
+pkg_license=('gplv2+')
+pkg_source="N/A"
+pkg_shasum="N/A"
+pkg_deps=(core/coreutils core/php core/which)
+pkg_build_deps=(core/composer)
+pkg_bin_dirs=(bin/vendor/bin)
+
+do_download() {
+    return 0
+}
+
+do_verify() {
+    return 0
+}
+
+do_unpack() {
+    return 0
+}
+
+do_build() {
+    composer global require drush/drush:8.*
+}
+
+do_install() {
+    cp -r $HOME/.composer/* $pkg_prefix/bin/
+    fix_interpreter $pkg_prefix/bin/vendor/bin/drush core/coreutils bin/env
+    fix_interpreter $pkg_prefix/bin/vendor/bin/drush.launcher core/coreutils bin/env
+}
